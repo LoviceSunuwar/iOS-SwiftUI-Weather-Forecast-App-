50620c6cdb0bfbe173f276a80bbcbd46df0efe5c
LoviceSunuwar <Lovicesunuwar@gmail.com>
2023-05-09 19:12:36 -0400

    Initial Commit

---
+++ WeatherAssignment/WeatherAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAssignmentApp.swift
+//  WeatherAssignment
+//
+//  Created by Lovice Sunuwar on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
